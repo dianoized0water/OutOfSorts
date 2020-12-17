4acae249b3259bb152be9aee10af691965a15535
diana_yang <dyang20@stuy.edu>
2020-12-17 12:28:22 -0500

    bubblesort fix

--- Sorts.java
+++ Sorts.java
@@ -1,12 +1,15 @@
 public class Sorts{
   public static void bubbleSort(int[] data){
     int temp = 0;
-    for (int i=0 ; i<data.length; i++){
+    boolean done = false;
+    for (int i=0 ; i<data.length && !done; i++){
+      done = true
       for (int j=1; j<data.length-i; j++){
         if (data[j-1]>data[j]){
           temp = data[j-1];
           data[j-1]=data[j];
           data[j]=temp;
+          done = false;
         }
       }
     }
